24f404b26bf8258db591a790fc2f3b0cb03126df
James Crowson <crowson.j@gmail.com>
2014-02-05 15:33:20 -0500

    Still working...

--- MultiplayerTest/Assets/Marco Polo/RandomMatchmaker.cs
+++ MultiplayerTest/Assets/Marco Polo/RandomMatchmaker.cs
@@ -26,10 +26,24 @@ public class RandomMatchmaker : MonoBehaviour {
 	void OnJoinedRoom()
 	{
 		GameObject monster = PhotonNetwork.Instantiate("monsterprefab", Vector3.zero, Quaternion.identity, 0);
-		CharacterControl controller = monster.GetComponent<CharacterControl>();
-		controller.enabled = true;
-		CharacterCamera camera = monster.GetComponent<CharacterCamera>();
-		camera.enabled = true;
+		//CharacterControl controller = monster.GetComponent<CharacterControl>();
+		//controller.enabled = true;
+		//CharacterCamera camera = monster.GetComponent<CharacterCamera>();
+		//camera.enabled = true;
+		FPSInputController fpsController = monster.GetComponent<FPSInputController>();
+		fpsController.enabled = true;
+
+		CharacterMotor motor = monster.GetComponent<CharacterMotor>();
+		motor.enabled = true;
+
+		PlatformInputController platformInputController = monster.GetComponent<PlatformInputController>();
+		platformInputController.enabled = true;
+
+		MouseLook mouseLook = monster.GetComponent<MouseLook>();
+		mouseLook.enabled = true;
+
+		Camera playerCamera = monster.GetComponent<Camera>();
+		playerCamera.enabled = true;
 	}
 	
 }
