5a121a4df54b6b4768f55e235574588a95a89e9c
James Crowson <crowson.j@gmail.com>
2014-02-05 13:48:55 -0500

    firstcommit

---
+++ MultiplayerTest/Assets/Marco Polo/RandomMatchmaker.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMatchmaker : MonoBehaviour {
+
+	void Start()
+	{
+		PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+	
+	void OnGUI()
+	{
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+
+	void OnJoinedLobby()
+	{
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	void OnPhotonRandomJoinFailed()
+	{
+		PhotonNetwork.CreateRoom(null);
+	}
+
+	void OnJoinedRoom()
+	{
+		GameObject monster = PhotonNetwork.Instantiate("monsterprefab", Vector3.zero, Quaternion.identity, 0);
+		CharacterControl controller = monster.GetComponent<CharacterControl>();
+		controller.enabled = true;
+		CharacterCamera camera = monster.GetComponent<CharacterCamera>();
+		camera.enabled = true;
+	}
+	
+}
