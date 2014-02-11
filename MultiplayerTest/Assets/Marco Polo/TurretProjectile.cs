7ac12dbf3d6e476df4951d95c2a3755f1ef33017
James Crowson <crowson.j@gmail.com>
2014-02-10 21:39:46 -0500

    updates

--- MultiplayerTest/Assets/Marco Polo/TurretProjectile.cs
+++ MultiplayerTest/Assets/Marco Polo/TurretProjectile.cs
@@ -4,19 +4,19 @@ using System.Collections;
 public class TurretProjectile : MonoBehaviour {
 
 	public float mySpeed = 10;
+	public float force = 10;
 	public float myRange = 5;
 	private float myDist;
 	private float velocityToHitPlayerBack = 15.0f;
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		transform.Translate(Vector3.forward * Time.deltaTime * mySpeed);
+	
+		 transform.Translate(Vector3.forward * Time.deltaTime * mySpeed);
 		myDist += Time.deltaTime * mySpeed;
 		if(myDist >= myRange){
 			Destroy(gameObject);
@@ -26,6 +26,9 @@ public class TurretProjectile : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other) {
 
+		//whatever it hits, delete it.
+		Destroy(gameObject);
+
 		if (other.tag == "Player") {
 
 			Debug.Log ("hit the player, knock back!");
@@ -34,5 +37,21 @@ public class TurretProjectile : MonoBehaviour {
 			//charMotor.SetVelocity(dir*velBack);
 			charMotor.SetVelocity(dir * velocityToHitPlayerBack);
 		}
+		else {
+			Debug.Log ("hit something else!");
+		}
 	}
+
+
+	void OnCollisionEnter(Collision collision){
+		//next - check if we have collided with anything but player/enemy
+		if(collision.gameObject.tag == "Player" || collision.gameObject.tag == "Enemy"){
+			//do nothing - or apply damage maybe?
+			Debug.Log ("");
+		}
+		else{
+			Destroy(gameObject);
+		}
+	}
+
 }
